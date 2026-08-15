7e001b1ac11e10879ff1b9423b98103d801c0189
Gonzalo <70833860+Gonzalo-W@users.noreply.github.com>
2026-08-14 20:20:05 -0500

    Add files via upload

---
+++ tipos_datos G.py
@@ -0,0 +1,30 @@
+#Tipo de datos
+# int : entero (1,2,3,4,5,......)
+# float : decimales (10.5,11.2,......)
+# str : cadena ("texto",'texto')
+# bool: boleano (true, false : 1 , 0)
+# array: arreglo ( nombres = ["redy","delgado","sequeiros"])
+
+#IF-ELSE
+# edad = 38
+# if(edad >= 18):
+#     print(f"El usuario es mayor de edad, por que tiene {edad} años")
+# else:
+#     print("El usuario es menor de edad.")
+
+#for : "Para" un número del 1 al 10 que haga tal cosa 
+# nombres = ["redy","delgado","sequeiros"] #array
+
+# for i in nombres:
+#     print(f"El nombre es: {i}")
+
+#while: "Mientras" se ejecuta de forma infinita mientras cumpla una condición
+# i = 2
+# while i <= 10: # 6 < 6
+#     print(f"se imprime {i}") #5
+#     i = i+1 #6
+
+#funcion : bloque de codigo reutilizable. "def"
+# def mi_nombre(x):
+#     return x
+# print(mi_nombre("algoritmica"))
