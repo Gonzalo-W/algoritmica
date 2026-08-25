05a0da80e500fd79641f65a185d1de1a44ddc628
Gonzalo <70833860+Gonzalo-W@users.noreply.github.com>
2026-08-25 07:54:35 -0500

    Add files via upload

---
+++ obtener primero.py
@@ -0,0 +1,17 @@
+#def obtener_primero(lista):
+   # return lista[0]
+#lista = [0, 1, 2, 3, 4, 5, 6]
+#print(obtener_primero(lista)) 
+
+
+#def obtener_primero(lista):
+ #   return lista[1]
+#lista_arreglo = [10,8,6,4,2,1]
+#print(F"El numero devuelto es: { obtener_primero(lista_arreglo)}") 
+
+
+def imprimir_lista(lista):
+    for elemento in lista:
+        print(elemento)
+        lista = ["pedro", "pablo", "maria" , "juana"]
+        imprimir_todos(lista)
